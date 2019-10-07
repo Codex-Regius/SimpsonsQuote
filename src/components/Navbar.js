223e24421022b8a39ceaa6b334ba8748be86fdbf
Martin Klauer <mdklauer@gmail.com>
2019-10-07 11:24:50 +0200

    test react

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar(){
+    return(
+    <nav className='Navbar'>
+        <h1>Hello World</h1>
+    </nav>
+    )
+};
+
+Navbar();
+
+export default Navbar
